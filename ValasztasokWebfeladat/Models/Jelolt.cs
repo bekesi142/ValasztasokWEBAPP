f21977c67e6f283d6d553e57b2a6733763a771c6
Békési Bálint <bekesi.balint@burg.lo.hu>
2024-09-25 11:04:39 +0200

    hoki

---
+++ ValasztasokWebfeladat/Models/Jelolt.cs
@@ -0,0 +1,11 @@
+﻿namespace ValasztasokWebfeladat.Models
+{
+    public class Jelolt
+    {
+        public int id { get; set; }
+        public string nev { get; set; }
+        public string kerulet { get; set; }
+        public string part { get; set; }
+
+    }
+}
---
+++ ValasztasokWebfeladat/Models/ValasztasokWebfeladat.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ValasztasokWebfeladat.Models
+{
+    public class ValasztasokWebfeladat :DbContext
+    {
+        public ValasztasokWebfeladat (DbContextOptions<ValasztasokWebfeladat> options): base(options) { }
+
+        public DbSet<Jelolt> Jeloltek { get; set; }
+        public DbSet<Part> Parts { get; set; }
+    }
+}
